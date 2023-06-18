baccce0ac1cf4f30f402657a8b24ab2b6ce51d3a
Jawid <41361157+infjay@users.noreply.github.com>
2023-06-18 19:15:16 +0200

    Hero section done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
 "use client"
 import React from 'react'
 import { Cursor,useTypewriter } from 'react-simple-typewriter'
+import BgCircles from './BgCircles'
+import thug from "../assets/thug.png"
+import Link from 'next/link'
 type Props = {}
 
 function Hero({}: Props) {
@@ -14,9 +17,31 @@ function Hero({}: Props) {
     delaySpeed:2000,
   })
     return (
-    <div>
-        <span>{text}</span>
+    <div className='h-screen flex flex-col space-y-8 items-center text-center justify-center overflow-hidden'>
+        <BgCircles />
+        <img src='https://i.seadn.io/gcs/files/ab8500681ef3049af50ffafcc4954b2b.png?auto=format&dpr=1&w=1920' alt="thug" 
+        className='relative rounded-full h-32 w-32 mx-auto object-cover' />
+       <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'> 
+        <span className='mr-3'>{text}</span>
         <Cursor cursorColor='#f7ab0a' />
+        </h1>
+        <div className='pt-5'>
+            <Link href="#about">
+            <button className='heroButton'>About</button>
+            </Link>
+            <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+            </Link>
+            <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+            </Link>
+            <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+            </Link>
+        </div>
+       </div>
     </div>
   )
 }
