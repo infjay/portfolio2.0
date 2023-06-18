7cd0dadc23ce26375b28d5a2d3b4d9525ec73a96
Jawid <41361157+infjay@users.noreply.github.com>
2023-06-18 19:15:37 +0200

    circles update

--- components/BgCircles.tsx
+++ components/BgCircles.tsx
@@ -20,10 +20,10 @@ const BgCircles = (props: Props) => {
     }}
     className='relative flex justify-center items-center'>
         <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-        <div className='absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52'/>
-        <div className='absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52'/>
-        <div className='absolute border border-[#F7AB0A] opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
-        <div className='absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52 '/>
+        <div className='absolute border border-red-400 rounded-full h-[300px] w-[300px] mt-52 animate-pulse'/>
+        <div className='absolute border border-yellow-600 rounded-full h-[500px] w-[500px] mt-52 animate-pulse'/>
+        <div className='absolute border border-green-400 opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
+        <div className='absolute border border-purple-400 rounded-full h-[800px] w-[800px] mt-52 animate-pulse '/>
     </motion.div>
   )
 }
