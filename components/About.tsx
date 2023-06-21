39f5d6228f8f97b8c5d0be920afa560290bb6ad2
Jawid <41361157+infjay@users.noreply.github.com>
2023-06-21 13:10:27 +0200

    about us responsive

--- components/About.tsx
+++ components/About.tsx
@@ -7,8 +7,8 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+    <div className="h-screen flex flex-col relative text-center md:text-left lg:flex-row  max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase max-sm:text-lg tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
       <motion.img
@@ -22,14 +22,14 @@ const About = (props: Props) => {
           duration: 1.2,
         }}
         src="https://i.seadn.io/gcs/files/ab8500681ef3049af50ffafcc4954b2b.png?auto=format&dpr=1&w=1920"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover max-sm:w-32 max-sm:h-32 md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
       />
-      <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">
+      <div className="space-y-10 max-sm:space-y-2 px-0 md:px-10  max-sm:pt-6">
+        <h4 className="text-4xl font-semibold max-sm:text-lg">
           Here is a{" "}
-          <span className="underline decoration-[#F7AB0A]">little</span> {""} Background
+          <span className="underline  decoration-[#F7AB0A]">little</span> {""} Background
         </h4>
-        <p className="text-base">
+        <p className="text-base sm:text-lg max-sm:text-sm">
           Hi there, I'm a web developer &. I specialize in developing beautiful
           and functional websites that help businesses achieve their goals. My
           expertise includes JS/TS/React/Node.js/Express.js, and I am passionate
