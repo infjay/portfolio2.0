ba4354bd8967574c37b5a45072f012dcbc3f7c38
Jawid <41361157+infjay@users.noreply.github.com>
2023-06-21 13:10:43 +0200

    Hero responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,25 +26,26 @@ function Hero({}: Props) {
         className='relative rounded-full h-32 w-32 mx-auto object-cover'
       />
       <div className='z-20'>
-        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 max-sm:text-[10px] tracking-[15px]'>Software Engineer</h2>
         <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
-          <span className='mr-3'>{text}</span>
+          <span className='mr-3 max-sm:text-lg'>{text}</span>
           <Cursor cursorColor='#f7ab0a' />
         </h1>
-        <div className='pt-5'>
-          <Link href="#about">
-            <button className='heroButton'>About</button>
-          </Link>
-          <Link href="#experience">
-            <button className='heroButton'>Experience</button>
-          </Link>
-          <Link href="#projects">
-            <button className='heroButton'>Projects</button>
-          </Link>
-          <Link href="#skills">
-            <button className='heroButton'>Skills</button>
-          </Link>
+        <div className="pt-5 max-sm:pt-20">
+        <Link href="#about">
+    <button className="heroButton max-sm:text-[8px]">About</button>
+  </Link>
+  <Link href="#experience">
+    <button className="heroButton max-sm:text-[8px]">Experience</button>
+  </Link>
+  <Link href="#projects">
+    <button className="heroButton max-sm:text-[8px]">Projects</button>
+  </Link>
+  <Link href="#skills">
+    <button className="heroButton max-sm:text-[8px]">Skills</button>
+  </Link>
         </div>
+        
       </div>
     </div>
   )
