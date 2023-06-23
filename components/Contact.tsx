ed3038e4587f60bc1948bc40a562f5468d3fe618
Jawid <41361157+infjay@users.noreply.github.com>
2023-06-23 23:57:08 +0200

    contact responsiveness

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -29,31 +29,31 @@ const Contact = (props: Props) => {
       className="h-screen flex relative flex-col text-center md:text-left md:flex-row
     max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute max-sm:text-sm top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
       <div className="flex flex-col space-y-10">
-        <h4 className="text-4xl font-semibold text-center">
+        <h4 className="text-4xl font-semibold text-center max-sm:text-sm">
           I have got just what you need.{" "}
           <span className="underline decoration-[#F7AB0A]/50">Let's Talk.</span>
         </h4>
-        <div className="space-y-10">
+        <div className="space-y-10 ">
           <div className="flex items-center space-x-5 justify-center">
-            <PhoneIcon className="text-orange-400 h-7 w-7 animate-pulse" />
-            <p>+4917640458314</p>
+            <PhoneIcon className="text-orange-400 h-7 w-7 max-sm:h-5 max-sm:w-5 animate-pulse" />
+            <p className="max-sm:text-sm" >+4917640458314</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
-            <EnvelopeIcon className="text-orange-400 h-7 w-7 animate-pulse" />
-            <p className="text-2xl">info@jawid.net</p>
+            <EnvelopeIcon className="text-orange-400 h-7 w-7 max-sm:h-5 max-sm:w-5 animate-pulse" />
+            <p className="text-2xl max-sm:text-sm">info@jawid.net</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
-            <MapPinIcon className="text-orange-400 h-7 w-7 animate-pulse" />
-            <p className="text-2xl">Berlin, Germany</p>
+            <MapPinIcon className="text-orange-400 h-7 w-7 max-sm:h-5 max-sm:w-5 animate-pulse" />
+            <p className="text-2xl max-sm:text-sm">Berlin, Germany</p>
           </div>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
-            <div className="flex space-x-2"> 
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto ">
+            <div className="flex max-sm:space-x-0 max-sm:space-y-2 space-x-2 max-sm:flex-col"> 
                 <input {...register("name")} placeholder="Name" className="contactInput" type="text" /> 
                 <input {...register("email")} placeholder="Email" className="contactInput" type="email" />
             </div>
