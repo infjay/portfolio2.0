c6df4a4e9d01fac2150edfe2fe66f3e0beb9a24f
Jawid <41361157+infjay@users.noreply.github.com>
2023-06-25 23:15:47 +0200

    update

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,10 +14,10 @@ const Skills = (props: Props) => {
     className='h-screen flex relative flex-col text-center md:text-left
      xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0
      mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        <h3 className='absolute top-24 max-sm:text-sm uppercase tracking-[20px] text-gray-500 text-2xl'>
             Skills
         </h3>
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current profiecieny</h3>
+        <h3 className='absolute top-36 uppercase max-sm:text-xs tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current profiecieny</h3>
         <div className='grid grid-cols-4 gap-5'>
             <Skill />
             <Skill />
@@ -33,7 +33,7 @@ const Skills = (props: Props) => {
             <Skill />
             <Skill />
             <Skill />
-            <Skill />
+           
        
         </div>
     </motion.div>
